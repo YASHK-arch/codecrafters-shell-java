0efbf80fbd5575590341784ac666f5eb37cae5d1
Yash Kumar <yashkumar02006@gmail.com>
2026-06-17 00:04:15 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,17 @@ public class Main {
             if (command.equals("exit")){
                 break;
             }
+            else if (command.startsWith("echo ")) {
+                String message = command.substring(5);
+                System.out.println(message);
+            }
+            // else if (command.equals("help")) {
+            //     System.out.println("Available commands:");
+            //     System.out.println("echo [message] - prints the message");
+            //     System.out.println("help - displays this help message");
+            //     System.out.println("exit - exits the program");
+            // }
+            else {
             System.out.println(command + ": command not found");
         }
 
