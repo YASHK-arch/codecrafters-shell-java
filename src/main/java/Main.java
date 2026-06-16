13d072a955bae683ccf36fc262c0b6d0044022e0
Yash Kumar <yashkumar02006@gmail.com>
2026-06-17 00:13:26 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -13,6 +14,18 @@ public class Main {
             } else if (command.startsWith("echo ")) {
                 String message = command.substring(5);
                 System.out.println(message);
+            } else if (command.startsWith("type")) {
+                HashSet<String> builtin = new HashSet<>();
+                builtin.add("echo");
+                builtin.add("type");
+                builtin.add("exit");
+                String message = command.substring(5);
+                if (builtin.contains(message)) {
+                    System.out.println(message + " is a shell builtin");
+                } else {
+                    System.out.println(message + ": not found");
+                }
+
             } else {
                 System.out.println(command + ": command not found");
             }
