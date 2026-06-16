5260c5741e9433022103a7272000e7073b754659
Yash Kumar <yashkumar02006@gmail.com>
2026-06-17 00:06:44 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,22 +8,15 @@ public class Main {
         while (true) {
             System.out.print("$ ");
             String command = sc.nextLine();
-            if (command.equals("exit")){
+            if (command.equals("exit")) {
                 break;
-            }
-            else if (command.startsWith("echo ")) {
+            } else if (command.startsWith("echo ")) {
                 String message = command.substring(5);
                 System.out.println(message);
+            } else {
+                System.out.println(command + ": command not found");
             }
-            // else if (command.equals("help")) {
-            //     System.out.println("Available commands:");
-            //     System.out.println("echo [message] - prints the message");
-            //     System.out.println("help - displays this help message");
-            //     System.out.println("exit - exits the program");
-            // }
-            else {
-            System.out.println(command + ": command not found");
-        }
 
+        }
     }
 }
